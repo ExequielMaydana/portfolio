e376b1da28f8768be434cde08e422f99eddd3769
ExeDev77 <exedevcoding22@gmail.com>
2024-10-04 01:11:01 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,29 +20,51 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="twitter:title" content="Exequiel Maydana | Desarrollador Web" />
-        <meta name="twitter:description" content="Experto en desarrollo web con especialización en Next.js. Ofrezco soluciones innovadoras y eficientes para proyectos web de alta calidad. Explora mi portafolio profesional para ver ejemplos de mi trabajo." />
+        <meta
+          name="twitter:title"
+          content="Exequiel Maydana | Desarrollador Web"
+        />
+        <meta
+          name="twitter:description"
+          content="Experto en desarrollo web con especialización en Next.js. Ofrezco soluciones innovadoras y eficientes para proyectos web de alta calidad. Explora mi portafolio profesional para ver ejemplos de mi trabajo."
+        />
         <meta name="twitter:image" content="/img/me.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="keywords" content="desarrollador web, Next.js, soluciones web, portafolio profesional, Exequiel Maydana" />
+        <meta
+          name="keywords"
+          content="desarrollador web, Next.js, soluciones web, portafolio profesional, Exequiel Maydana"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="language" content="es" />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://exequielmaydana.vercel.app/" />
-        <meta property="og:title" content="Exequiel Maydana | Desarrollador Web" />
-        <meta property="og:description" content="Experto en desarrollo web con especialización en Next.js. Ofrezco soluciones innovadoras y eficientes para proyectos web de alta calidad. Explora mi portafolio profesional para ver ejemplos de mi trabajo." />
-        <meta property="og:image" content="https://exequielmaydana.vercel.app/img/me.png" />
+        <meta
+          property="og:title"
+          content="Exequiel Maydana | Desarrollador Web"
+        />
+        <meta
+          property="og:description"
+          content="Experto en desarrollo web con especialización en Next.js. Ofrezco soluciones innovadoras y eficientes para proyectos web de alta calidad. Explora mi portafolio profesional para ver ejemplos de mi trabajo."
+        />
+        <meta
+          property="og:image"
+          content="https://exequielmaydana.vercel.app/img/me.png"
+        />
 
         <link rel="canonical" href="https://exequielmaydana.vercel.app/" />
         <meta name="robots" content="index, follow" />
 
-        <meta name="DC.title" content="Exequiel Maydana | Desarrollador Web Senior" />
+        <meta
+          name="DC.title"
+          content="Exequiel Maydana | Desarrollador Web Senior"
+        />
         <meta name="DC.creator" content="Exequiel Maydana" />
-        <meta name="DC.description" content="Experto en desarrollo web con especialización en Next.js. Ofrezco soluciones innovadoras y eficientes para proyectos web de alta calidad. Explora mi portafolio profesional para ver ejemplos de mi trabajo." />
+        <meta
+          name="DC.description"
+          content="Experto en desarrollo web con especialización en Next.js. Ofrezco soluciones innovadoras y eficientes para proyectos web de alta calidad. Explora mi portafolio profesional para ver ejemplos de mi trabajo."
+        />
         <meta name="DC.language" content="es" />
-
-
       </head>
       <body className={`${montserrat.className} antialiased`}>
         {" "}
